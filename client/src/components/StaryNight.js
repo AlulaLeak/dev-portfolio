782893b8e4d4b82b076b71c413301fad5e875d73
AlulaLeak <alula.leakemariam94@gmail.com>
2022-06-30 22:12:10 -0400

    converted starysky into functional component and refactored functions and states into customhook useStars

--- client/src/components/StaryNight.js
+++ client/src/components/StaryNight.js
@@ -1,122 +1,52 @@
-import React from "react";
-import anime from "animejs/lib/anime.es.js";
+import React, { useEffect } from "react";
+import useStars from "../hooks/useStars";
 import "../styles/StaryNight.css";
 
-export default class StarrySky extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isMounted: false,
-      num: 600,
-      vw: Math.max(
-        document.documentElement.clientWidth,
-        window.innerWidth || 0
-      ),
-      vh: Math.max(
-        (document.documentElement.clientHeight, window.innerHeight || 0)
-      ),
-    };
-  }
-  starryNight = () => {
-    anime({
-      targets: ["#sky .star"],
-      opacity: [
-        {
-          duration: 700,
-          value: "0",
-        },
-        {
-          duration: 700,
-          value: "1",
-        },
-      ],
-      easing: "linear",
-      loop: true,
-      delay: (el, i) => 50 * i,
-    });
-  };
-  shootingStars = () => {
-    anime.remove("#shootingstars .wish");
-    anime({
-      targets: ["#shootingstars .wish"],
-      easing: "linear",
-      loop: true,
-      delay: (el, i) => 1000 * i,
-      opacity: [
-        {
-          duration: 700,
-          value: "1",
-        },
-      ],
-      width: [
-        {
-          value: "150px",
-        },
-        {
-          value: "0px",
-        },
-      ],
-      translateX: 350,
-    });
-  };
-  randomRadius = () => {
-    return Math.random() * 0.7 + 0.6;
-  };
-  getRandomX = () => {
-    return Math.floor(Math.random() * Math.floor(this.state.vw)).toString();
-  };
-  getRandomY = () => {
-    return Math.floor(Math.random() * Math.floor(this.state.vh)).toString();
-  };
-  renderShootingStars = async () => {
-    this.setState({
-      isMounted: true,
-    });
-  };
-  componentDidMount() {
-    this.renderShootingStars().then(() => {
-      this.starryNight();
-      this.shootingStars();
-    });
-  }
-  componentWillUnmount() {
-    this.setState({
-      isMounted: false,
-    });
-  }
-  render() {
-    const { num } = this.state;
-    return (
-      <div id="App">
-        <svg id="sky">
-          {[...Array(num)].map((x, y) => (
-            <circle
-              cx={this.getRandomX()}
-              cy={this.getRandomY()}
-              r={this.randomRadius()}
-              stroke="none"
-              strokeWidth="0"
-              fill="white"
-              key={y}
-              className="star"
-            />
-          ))}
-        </svg>
-        {this.state.isMounted === true && (
-          <div id="shootingstars">
-            {[...Array(60)].map((x, y) => (
-              <div
-                key={y}
-                className="wish"
-                style={{
-                  left: `${this.getRandomY()}px`,
-                  top: `${this.getRandomX()}px`,
-                }}
-              />
-            ))}
-          </div>
-        )}
+export default function StarrySky() {
+  const {
+    getRandomX,
+    getRandomY,
+    randomRadius,
+    shootingStars,
+    starryNight,
+    num,
+    isMounted,
+  } = useStars();
+
+  useEffect(() => {
+    starryNight();
+    shootingStars();
+  }, [shootingStars, starryNight]);
+
+  return (
+    <div id="App">
+      <svg id="sky">
+        {[...Array(num)].map((x, y) => (
+          <circle
+            cx={getRandomX()}
+            cy={getRandomY()}
+            r={randomRadius()}
+            stroke="none"
+            strokeWidth="0"
+            fill="white"
+            key={y}
+            className="star"
+          />
+        ))}
+      </svg>
+      {isMounted === true && <div style={{ color: "white" }}>hi</div>}
+      <div id="shootingstars">
+        {[...Array(60)].map((x, y) => (
+          <div
+            key={y}
+            className="wish"
+            style={{
+              left: `${getRandomY()}px`,
+              top: `${getRandomX()}px`,
+            }}
+          />
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
 }
---
+++ client/src/hooks/useStars.js
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import anime from "animejs/lib/anime.es.js";
+
+export default function useStars() {
+  const [isMounted, setIsMounted] = useState(false);
+  const [num, setNum] = useState(600);
+  const [vw, setVw] = useState(
+    Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
+  );
+  const [vh, setVh] = useState(
+    Math.max((document.documentElement.clientHeight, window.innerHeight || 0))
+  );
+  const starryNight = () => {
+    anime({
+      targets: ["#sky .star"],
+      opacity: [
+        {
+          duration: 700,
+          value: "0",
+        },
+        {
+          duration: 700,
+          value: "1",
+        },
+      ],
+      easing: "linear",
+      loop: true,
+      delay: (el, i) => 50 * i,
+    });
+  };
+  const shootingStars = () => {
+    console.log("shootingStars");
+    anime.remove("#shootingstars .wish");
+    anime({
+      targets: ["#shootingstars .wish"],
+      easing: "linear",
+      loop: true,
+      delay: (el, i) => 1000 * i,
+      opacity: [
+        {
+          duration: 700,
+          value: "1",
+        },
+      ],
+      width: [
+        {
+          value: "150px",
+        },
+        {
+          value: "0px",
+        },
+      ],
+      translateX: 350,
+    });
+  };
+  const randomRadius = () => {
+    return Math.random() * 0.7 + 0.6;
+  };
+  const getRandomX = () => {
+    return Math.floor(Math.random() * Math.floor(vw)).toString();
+  };
+  const getRandomY = () => {
+    return Math.floor(Math.random() * Math.floor(vh)).toString();
+  };
+
+  return {
+    getRandomX,
+    getRandomY,
+    randomRadius,
+    shootingStars,
+    starryNight,
+    vh,
+    setVh,
+    setVw,
+    setNum,
+    vw,
+    num,
+    isMounted,
+    setIsMounted,
+  };
+}
